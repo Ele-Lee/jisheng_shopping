41219b68a2d063dc9cdc6c5d0a91ba0603be21c0
Eli Li <elili@shein.com>
2025-10-30 16:13:55 +0800

    db

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg'
 
 let pool: Pool | null = null
-
+console.log('%celelee test database url:', 'color:#fff;background:#000', process.env.DATABASE_URL)
 export function getPool() {
   if (!pool) {
     pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URL || 'postgresql://postgres:123456@localhost:5432/postgres',
     })
   }
   return pool
